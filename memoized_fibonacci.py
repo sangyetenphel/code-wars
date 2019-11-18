adb1134da08435fb037cfc1c94db46effaec5ec9
sangyetenphel <sangyetenphel@gmail.com>
2019-11-17 21:41:05 -0500

    DAY13 decorators

---
+++ memoized_fibonacci.py
@@ -0,0 +1,28 @@
+##cache = {0:0, 1:1}
+##def fibonacci(n):
+##    if n in cache:
+##        return cache[n]
+##    else:
+##        cache[n] =  fibonacci(n - 1) + fibonacci(n - 2)
+##        return cache[n]
+##    
+##
+##print(fibonacci(50))
+
+
+def memoized(f):
+    cache = {}
+    def wrapped(k):
+        v = cache.get(k)
+        if v is None:
+            v = cache[k] = f(k)
+        return v
+    return wrapped
+
+@memoized
+def fibonacci(n):
+    if n in [0, 1]:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(9))
